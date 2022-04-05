c0c71a8769cc15c8621f764e9835c9be9f9d081e
mfikri1689 <mfikri1689@gmail.com>
2022-04-05 15:48:57 +0700

    running detection + pop up

--- app/src/main/java/com/example/detectionapp/CameraActivity2.java
+++ app/src/main/java/com/example/detectionapp/CameraActivity2.java
@@ -5,20 +5,26 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class CameraActivity2 extends AppCompatActivity {
     Camera camera;
     FrameLayout frameLayout;
     ShowCamera showCamera;
-
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera2);
+        myDialog = new Dialog(this);
         if (ContextCompat.checkSelfPermission(CameraActivity2.this,
                 Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(CameraActivity2.this,
@@ -35,6 +41,31 @@ public class CameraActivity2 extends AppCompatActivity {
         showCamera = new ShowCamera(this,camera);
         frameLayout.addView(showCamera);
 
+    }
+    public void ShowPopup(View v){
+        Button cancel;
+        Button confirm;
+        myDialog.setContentView(R.layout.custompopup);
+        cancel = (Button) myDialog.findViewById(R.id.cancel);
+        confirm = (Button) myDialog.findViewById(R.id.confirm);
+        cancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                myDialog.dismiss();
+            }
+        });
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
 
+
+        myDialog.show();
+    }
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(switchActivityIntent);
     }
 }
